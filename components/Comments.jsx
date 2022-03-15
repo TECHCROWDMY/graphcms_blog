8307a60391da15144ffe3ffd477ecfc960ba7b78
Thilak Sundaram <techcrowdmy@gmail.com>
2022-03-15 22:09:04 +0800

    I have removed the env

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react';
 import moment from 'moment';
 import parse from 'html-react-parser';
 import { getComments } from '../services';
-import { comment } from 'postcss';
 
 const Comments = ({slug}) => {
   const [comments, setComments] = useState([])
